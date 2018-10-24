2dbedbbd7136cced6c53713e3e7e53c0c0610f45
Hunter Earley <earleyhunter@gmail.com>
2018-10-24 15:41:51 -0400

    This is my commit message

---
+++ src/githubexercise4/GitHubExercise4.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubexercise4;
+
+/**
+ *
+ * @author earle
+ */
+public class GitHubExercise4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        printName();
+    }
+    
+    public static void printName(){
+        System.out.println("My name is Hunter Earley and this is my statement");
+    }
+    
+}
